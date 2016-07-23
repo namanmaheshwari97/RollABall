986d9ac979e07a9be738c7e29fa87dd0eb86c3fa
Naman Maheshwari <namanmaheshwari97@gmail.com>
2016-07-23 20:21:25 +0530

    Created the basic game in Unity

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+			Application.Quit(); 
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.acceleration.x * 5;
+		float moveVertical = Input.acceleration.y * 5;
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count= count+1;
+			setCountText ();
+		}
+	}
+
+	void setCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 17) {
+			winText.text = "You win.";
+		}
+	}
+}
---
+++ Roll a Ball/Assets/Scripts/rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(45,45,45) * Time.deltaTime);
+	}
+}
